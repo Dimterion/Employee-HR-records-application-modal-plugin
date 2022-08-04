db6bb16317794bbff7b1963a1ad7a2b4243e263c
Dimterion <dimterion59@gmail.com>
2022-08-04 10:35:39 +0200

    Modal plugin code.

---
+++ src/lib/ModalPlugin.js
@@ -0,0 +1,16 @@
+import "./modalPlugin.css";
+
+function ModalPlugin({ showModal, modalText }) {
+  return (
+    <div className="modal-container" onClick={() => showModal(false)}>
+      <div className="modal-window" onClick={(e) => e.stopPropagation()}>
+        <p className="modal-text">{modalText}</p>
+        <button className="modal-btn" onClick={() => showModal(false)}>
+          X
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ModalPlugin;
