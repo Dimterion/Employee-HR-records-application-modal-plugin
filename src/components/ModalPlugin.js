2b48f82599e0d55acbe64c811ad18a5986ae8ff5
Dimterion <dimterion59@gmail.com>
2022-08-09 14:35:28 +0200

    Rearranged the styling method.

--- src/components/ModalPlugin.js
+++ src/components/ModalPlugin.js
@@ -1,51 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ModalPlugin({ showModal, modalText }) {
-  // Adding the styles
-  const modalContainerStyle = {
-    position: "fixed",
-    width: "100vw",
-    height: "100vh",
-    top: "50%",
-    left: "50%",
-    zIndex: "2",
-    transform: "translate(-50%, -50%)",
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
-  };
-
-  const modalWindowStyle = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  };
-
-  const modalTextStyle = {
-    width: "200px",
-    zIndex: "3",
-    borderRadius: "10px",
-    boxShadow: "0 5px 20px 0 rgba(0, 0, 0, 0.5)",
-    padding: "20px",
-    backgroundColor: "#fff",
-    fontSize: "1.5rem",
-    textAlign: "center",
-  };
-
-  const modalBtnStyle = {
-    cursor: "pointer",
-    position: "fixed",
-    top: "22%",
-    left: "98%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "50%",
-    backgroundColor: "#000",
-    color: "#fff",
-    padding: "5px 8px",
-    border: "1px solid #000",
-    fontWeight: "bold",
-  }
-
-  // Rendering a component with a modal window mechanic
+// Rendering a component with a modal window mechanic
+function ModalPlugin({
+  showModal,
+  modalText,
+  modalContainerStyle,
+  modalWindowStyle,
+  modalTextStyle,
+  modalBtnStyle,
+}) {
   return (
     <div style={modalContainerStyle} onClick={() => showModal(false)}>
       <div style={modalWindowStyle} onClick={(e) => e.stopPropagation()}>
@@ -57,5 +21,57 @@ function ModalPlugin({ showModal, modalText }) {
     </div>
   );
 }
+
+// Setting propTypes and defaultProps
+ModalPlugin.propTypes = {
+  showModal: PropTypes.bool.isRequired,
+  modalText: PropTypes.string.isRequired,
+  modalContainerStyle: PropTypes.object,
+  modalWindowStyle: PropTypes.object,
+  modalTextStyle: PropTypes.object,
+  modalBtnStyle: PropTypes.object,
+};
+
+ModalPlugin.defaultProps = {
+  modalContainerStyle: {
+    position: "fixed",
+    width: "100vw",
+    height: "100vh",
+    top: "50%",
+    left: "50%",
+    zIndex: "2",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+  modalWindowStyle: {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  modalTextStyle: {
+    width: "200px",
+    zIndex: "3",
+    borderRadius: "10px",
+    boxShadow: "0 5px 20px 0 rgba(0, 0, 0, 0.5)",
+    padding: "20px",
+    backgroundColor: "#fff",
+    fontSize: "1.5rem",
+    textAlign: "center",
+  },
+  modalBtnStyle: {
+    cursor: "pointer",
+    position: "fixed",
+    top: "22%",
+    left: "98%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "50%",
+    backgroundColor: "#000",
+    color: "#fff",
+    padding: "5px 8px",
+    border: "1px solid #000",
+    fontWeight: "bold",
+  },
+};
 
 export default ModalPlugin;
